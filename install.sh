2603f493b69b3d24d866d595abff5e4b4815b3a7
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2016-12-19 15:36:37 -0300

    Updating install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
+sudo npm install -g buffer-shims
+sudo npm install -g angular-cli
+sudo npm install -g alm
+sudo npm install -g typings
+sudo npm install -g tslint
+sudo npm install -g typescript
+sudo npm install -g typings
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 cd angular
 npm install
 npm run typings install
-sudo npm install -g alm
+#npm run typings -- install debug --save
+#npm run typings -- install dt~jasmine --save --global
+#npm run typings -- install dt~node --save --global
+#npm run typings -- install dt~core-js --save --global
 
