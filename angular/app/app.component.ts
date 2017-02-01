b995035272c246dc168b0d01c3ad56c96fa5d3e3
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2017-01-31 22:30:58 -0300

    Deleting angular folder, we are using a2cli now, angular-cli 1.0.24

--- angular/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-//import AbstractForm from '../components/AbstractForm';
-
-@Component({
-  selector: 'moldeo',
-  template: '<h1>Interface</h1>'
-})
-export class AppComponent { }
