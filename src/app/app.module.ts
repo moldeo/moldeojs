763bb35e1dfc98fa6fecefd5141e75d43a6717c5
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2018-03-03 08:01:55 -0300

    added socket io speak https

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,7 @@ const appRoutes: Routes = [
 import { SocketIoModule, SocketIoConfig} from 'ng-socket-io';
 
 
-const config: SocketIoConfig = { url: 'https://collaborative.moldeo.org', options: {rejectUnauthorized: false} };
+const config: SocketIoConfig = { url: 'https://collaborative.moldeo.org:8988', options: {rejectUnauthorized: false} };
 
 @NgModule({
   declarations: [
