6bdfb5a392979eab592133b25bbefcfd146764dc
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2018-03-03 07:09:20 -0300

    added socket io speak

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,7 @@ const appRoutes: Routes = [
 import { SocketIoModule, SocketIoConfig} from 'ng-socket-io';
 
 
-const config: SocketIoConfig = { url: 'http://localhost:8988', options: {} };
+const config: SocketIoConfig = { url: 'http://speak.moldeo.org:8988', options: {} };
 
 @NgModule({
   declarations: [
