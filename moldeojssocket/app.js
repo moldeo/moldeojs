9f101b85f9179a3012cf3e4d5403b2a4644b67c9
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2018-03-03 07:59:41 -0300

    added socket io speak https

--- moldeojssocket/app.js
+++ moldeojssocket/app.js
@@ -9,6 +9,14 @@ var fs = require("fs");
 
 app.use(express.static(path.join(__dirname, 'public/dist')));
 
+app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.header("Access-Control-Allow-Headers", "Content-Type");
+        res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+        next();
+    });
+
 app.get('*', function(req, res, next) {
   res.sendFile(__dirname+"/public/dist/index.html");
 });
