f2be21fab634a698d5adfe4886034409f45aee26
Ignacio <ibuioli@gmail.com>
2018-08-06 16:10:58 -0300

    Update angularJSON and Concatenate cause name

--- concatenate.js
+++ concatenate.js
@@ -2,13 +2,13 @@ const fs = require('fs-extra');
 const concat = require('concat');
 (async function build() {
 const files = [
-'./dist/elements/runtime.js',
-'./dist/elements/polyfills.js',
-'./dist/elements/scripts.js',
-'./dist/elements/main.js',
+'./dist/moldeojs/runtime.js',
+'./dist/moldeojs/polyfills.js',
+'./dist/moldeojs/scripts.js',
+'./dist/moldeojs/main.js',
 ]
 await fs.ensureDir('elements')
 await concat(files, 'elements/moldeojs-element.js');
-await fs.copyFile('./dist/elements/styles.css', 'elements/styles.css')
-await fs.copy('./dist/elements/assets/', 'elements/assets/' )
+await fs.copyFile('./dist/moldeojs/styles.css', 'elements/styles.css')
+await fs.copy('./dist/moldeojs/assets/', 'elements/assets/' )
 })()
